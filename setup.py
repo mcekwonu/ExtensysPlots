f62e6375eee0e0abec7366ce5d0fa0e1d065b96e
mirst <cekwonu@gmail.com>
2021-03-03 15:01:58 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Install Originpro style plotting
+"""Install OriginproPlot.
 
 This will copy the *.mplstyle files into the right directory.
 
@@ -15,14 +15,16 @@ from setuptools import setup
 from setuptools.command.install import install
 
 
-root = os.path.abspath(os.path.dirname(__file__))
+package_name = 'OriginproPlot'
 
-with open(os.path.join(root, 'README.md'), 'r', encoding='utf-8') as f:
-    long_description = f.read()
+# Get the description from README
+root = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(root, 'README.md'), 'r', encoding='utf-8') as fb:
+    long_description = fb.read()
 
 
 def install_styles():
-    stylefiles = glob.glob('styles/**/*.mplstyle', recursive=True)
+    style_files = glob.glob('styles/**/*.mplstyle', recursive=True)
 
     # find stylelib directory, where the *.mplstyle file goes
     mpl_stylelib_dir = os.path.join(matplotlib.get_configdir(), "stylelib")
@@ -31,11 +33,11 @@ def install_styles():
 
     # copy files over
     print("Installing styles into", mpl_stylelib_dir)
-    for stylefile in stylefiles:
-        print(os.path.basename(stylefile))
+    for style_file in style_files:
+        print(os.path.basename(style_file))
         shutil.copy(
-            stylefile,
-            os.path.join(mpl_stylelib_dir, os.path.basename(stylefile)))
+            style_file,
+            os.path.join(mpl_stylelib_dir, os.path.basename(style_file)))
 
 
 class PostInstallation(install):
@@ -45,20 +47,20 @@ class PostInstallation(install):
 
 
 setup(
-    name='OriginproPlot',
+    name=package_name,
     version='1.0.0',
     author="Chukwuemeka Michael Ekwonu",
-    author_email="michael.ekwonu@gmail.com",
-    description="Format Matplotlib for originpro style plotting",
+    author_email="mersthub@gmail.com",
+    description="Matplotlib format for originpro style plots",
     long_description=long_description,
     long_description_content_type='text/markdown',
     license='MIT',
     keywords=[
-        "matplotlib-style-sheets",
-        "matplotlib-figures",
-        "originpro-style",
+        "originproplot",
+        "matplotlib-style-sheet",
         "scientific-papers",
         "matplotlib-styles",
+        "matplotlib-figures",
         "python"
     ],
     url="https://github.com/mcekwonu/OriginproPlot/",
